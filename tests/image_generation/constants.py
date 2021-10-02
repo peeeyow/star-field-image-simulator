4dc5ec0df8e46c09ec40355a8d9cc8853a6e69f9
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2021-10-02 16:12:19 +0800

    added datapath

--- tests/image_generation/constants.py
+++ tests/image_generation/constants.py
@@ -6,4 +6,11 @@ Constants
 REL : float
     almost equal resolution
 """
+from pathlib import Path
+
+
+"tolerance for floating point comparisons"
 REL = 1e-4
+
+"database path"
+DATA_PATH = Path(__file__).parent.parent / "data"
